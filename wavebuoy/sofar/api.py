036121978c3c8b36b085ce25675e5c1258b275a1
Thiago Caminha <00116827@uwa.edu.au>
2025-01-09 17:02:57 +0800

    new sofar api module started using pysofar library

---
+++ wavebuoy/sofar/api.py
@@ -0,0 +1,54 @@
+from pysofar.sofar import SofarApi
+from pysofar.spotter import Spotter
+import pandas as pd
+
+import time
+
+
+class SofarAPI:
+    def __init__(self, buoys_metadata:pd.DataFrame):
+        self.buoys_metadata = buoys_metadata
+        self.api = self._initiate_sofar_api()
+        self.spotter_grid = self._load_spotter_grid()
+        self.devices = self._load_spotter_devices()
+
+    def _get_first_token(self):
+        return (self.buoys_metadata
+                        .sort_values("sofar_token")
+                        .iloc[0]
+                        .sofar_token
+        )
+        
+    def _initiate_sofar_api(self):
+        return SofarApi(custom_token=self._get_first_token())
+    
+    def _reload_sofar_api(self, token:str):
+        return SofarApi(custom_token=token)
+
+    def _validate_token(self, token:str) -> bool:
+        if not isinstance(token, str) or len(token) != 30:
+            raise ValueError("provide a valid token.")
+
+    def check_token_iteration(self, next_token:str) -> bool:
+        
+        self._validate_token(token=next_token)
+
+        if next_token != self.api.token:
+            self.api = self._reload_sofar_api(token=next_token)
+            self.spotter_grid = self._load_spotter_grid()
+            self.devices = self._load_spotter_devices()
+        
+    def get_spot_id(self, site_id:str, buoys_metadata:pd.DataFrame) -> str:
+        return buoys_metadata.loc[site_id,"serial"]
+
+    def _load_spotter_grid(self) -> list:
+        return self.api.get_spotters()
+    
+    def _load_spotter_devices(self):
+        return self.api.devices
+    
+    def select_spotter_obj_from_spotter_grid(self, spot_id:str, spotter_grid:list, devices:list) -> Spotter:
+        for device in devices:
+            if device["spotterId"] == spot_id:
+                idx = devices.index(device)
+        return spotter_grid[idx]
