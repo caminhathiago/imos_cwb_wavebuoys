dc90fca1e2821978db350cd6a986a3cdf7794525
Thiago Caminha <caminha.thiago@gmail.com>
2025-01-14 12:29:21 +0800

    tests first push

---
+++ tests/test_wavebuoy.py
@@ -0,0 +1,14 @@
+import pytest
+from wavebuoy.wavebuoy import WaveBuoy
+
+
+# def test_WaveBuoy():
+#     wv = WaveBuoy(buoy_type="sofar", buoys_metadata_file_name="buoys_metadata.csv")
+#     assert True
+
+def test_get_site_ids():
+    wv = WaveBuoy(buoy_type="sofar", buoys_metadata_file_name="buoys_metadata.csv")
+    # assert type(wv.site_ids) is list
+    print(wv.site_ids[0])
+    assert type(wv.site_ids[0]) is dict
+
