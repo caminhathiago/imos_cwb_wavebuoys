0d7bd70b0b7ce5c17ccc5c47354acddc0fc0e7a3
Thiago Caminha <caminha.thiago@gmail.com>
2025-01-14 15:07:05 +0800

    nc files template considering institution, datetime handling changed

--- wavebuoy/config/config.py
+++ wavebuoy/config/config.py
@@ -1,3 +1,5 @@
 FILES_OUTPUT_PATH = r"C:\Users\00116827\cwb\wavebuoy_aodn\output_path"
 FILES_PATH = r"C:\Users\00116827\cwb\wavebuoy_aodn\wavebuoy"
-NC_FILE_NAME_TEMPLATE = "{institution}_{date_time_month}_{site_id}_RT_WAVE-PARAMETERS_monthly.nc"
+NC_FILE_NAME_TEMPLATE = "{institution}_{year}{month}01_{site_id}_RT_WAVE-PARAMETERS_monthly.nc"
+
+REGION_TO_INSTITUTION = {"VIC":"VIC-DEAKIN-UNI"}
