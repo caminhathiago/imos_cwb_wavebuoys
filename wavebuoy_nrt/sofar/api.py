fb70cf9dea3084f778783c91e80f04e7cac6b28e
Thiago Caminha <caminha.thiago@gmail.com>
2025-01-15 15:19:48 +0800

    methods to convert data added, probably best moving to wavebuoy module

--- wavebuoy_nrt/sofar/api.py
+++ wavebuoy_nrt/sofar/api.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 class SofarAPI:
-    def __init__(self, buoys_metadata:pd.DataFrame):
+    def __init__(self, buoys_metadata: pd.DataFrame):
         self.buoys_metadata = buoys_metadata
         self.api = self._initiate_sofar_api()
         self.spotter_grid = self._load_spotter_grid()
@@ -22,14 +22,14 @@ class SofarAPI:
     def _initiate_sofar_api(self):
         return SofarApi(custom_token=self._get_first_token())
     
-    def _reload_sofar_api(self, token:str):
+    def _reload_sofar_api(self, token: str):
         return SofarApi(custom_token=token)
 
-    def _validate_token(self, token:str) -> bool:
+    def _validate_token(self, token: str) -> bool:
         if not isinstance(token, str) or len(token) != 30:
             raise ValueError("provide a valid token.")
 
-    def check_token_iteration(self, next_token:str) -> bool:
+    def check_token_iteration(self, next_token: str) -> bool:
         
         self._validate_token(token=next_token)
 
@@ -38,7 +38,7 @@ class SofarAPI:
             self.spotter_grid = self._load_spotter_grid()
             self.devices = self._load_spotter_devices()
         
-    def get_spot_id(self, site_id:str, buoys_metadata:pd.DataFrame) -> str:
+    def get_spot_id(self, site_id: str, buoys_metadata: pd.DataFrame) -> str:
         return buoys_metadata.loc[site_id,"serial"]
 
     def _load_spotter_grid(self) -> list:
@@ -47,7 +47,10 @@ class SofarAPI:
     def _load_spotter_devices(self):
         return self.api.devices
     
-    def select_spotter_obj_from_spotter_grid(self, spot_id:str, spotter_grid:list, devices:list) -> Spotter:
+    def select_spotter_obj_from_spotter_grid(self, 
+                                             spot_id: str, 
+                                             spotter_grid: list,
+                                             devices: list) -> Spotter:
         for device in devices:
             if device["spotterId"] == spot_id:
                 idx = devices.index(device)
@@ -56,22 +59,15 @@ class SofarAPI:
     def get_latest_available_datetime(self, spotter_obj) -> datetime:
         latest_available_datetime = spotter_obj.latest_data()["wave"]["timestamp"]
         return datetime.strptime(latest_available_datetime, "%Y-%m-%dT%H:%M:%S.%fZ")
+
     
-    def grab_raw_data(self, spotter_obj, include_bulk:bool=True, include_spectral:bool=False) -> pd.DataFrame:
+    def convert_raw_dataframe(self, raw_data: dict, parameters_type: str) -> pd.DataFrame: 
+        return pd.DataFrame(raw_data[parameters_type])
+
+    def merge_parameter_types(self, waves: pd.DataFrame, wind: pd.DataFrame) -> pd.DataFrame:
+        # # IN PROGRESS
+        # wind = wind.drop(columns=["latitude","longitude", "processing_source"])
+        # all = waves.merge(wind, on="timestamp", copy=False)
         
-        # TEMPORARY - Working with kwargs as possible approach
-        # def function_x(argument1, argument2):
-        #     return argument1 + argument2
-        # kwargs = {"argument1":2, "argument2":2}
-        # function_x(**kwargs) -> 4
-        
-        kwargs = {}
-
-        if include_bulk:
-            pass
-        if include_spectral:
-            pass
-
-        data = spotter_obj.grab_data(**kwargs)
-
-        return data
+        # return waves
+        pass
