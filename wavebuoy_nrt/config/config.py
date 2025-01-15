1aa24941744d531ec50d06d386c38228bc4fd7ad
Thiago Caminha <caminha.thiago@gmail.com>
2025-01-15 15:20:53 +0800

    netcdf file name template changed

--- wavebuoy_nrt/config/config.py
+++ wavebuoy_nrt/config/config.py
@@ -1,5 +1,5 @@
 FILES_OUTPUT_PATH = r"C:\Users\00116827\cwb\wavebuoy_aodn\output_path"
-FILES_PATH = r"C:\Users\00116827\cwb\wavebuoy_aodn\wavebuoy"
-NC_FILE_NAME_TEMPLATE = "{institution}_{year}{month}01_{site_id}_RT_WAVE-PARAMETERS_monthly.nc"
+FILES_PATH = r"C:\Users\00116827\cwb\wavebuoy_aodn\wavebuoy_nrt"
+NC_FILE_NAME_TEMPLATE = "{institution}_{monthly_datetime}_{site_id}_RT_WAVE-PARAMETERS_monthly.nc"
 
 REGION_TO_INSTITUTION = {"VIC":"VIC-DEAKIN-UNI"}
