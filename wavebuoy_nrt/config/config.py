3e1d835b31aea97a8a42aaa109071471b0ef9d2c
Thiago Caminha <caminha.thiago@gmail.com>
2025-01-16 15:35:38 +0800

    aodn variables naming convention added

--- wavebuoy_nrt/config/config.py
+++ wavebuoy_nrt/config/config.py
@@ -3,3 +3,32 @@ FILES_PATH = r"C:\Users\00116827\cwb\wavebuoy_aodn\wavebuoy_nrt"
 NC_FILE_NAME_TEMPLATE = "{institution}_{monthly_datetime}_{site_id}_RT_WAVE-PARAMETERS_monthly.nc"
 
 REGION_TO_INSTITUTION = {"VIC":"VIC-DEAKIN-UNI"}
+
+# AODN_COLUMNS_TEMPLATE = {
+#     'TIME': "timestamp",
+#     'timeSeries': None,
+#     'LATITUDE': "longitude",
+#     'LONGITUDE': "latitude",
+#     'SSWMD': "meanDirection",
+#     'WAVE_quality_control': None,
+#     'WMDS': "meanDirectionalSpread",
+#     'WPDI': "peakDirection",
+#     'WPDS': "peakDirectionalSpread",
+#     'WPFM': "meanPeriod",
+#     'WPPE': "peakPeriod",
+#     'WSSH': "significantWaveHeight"
+# }
+
+AODN_COLUMNS_TEMPLATE = {
+    "timestamp": 'TIME',
+    # None: ['timeSeries', 'WAVE_quality_control'],
+    "longitude": 'LONGITUDE',
+    "latitude": 'LATITUDE',
+    "meanDirection": 'SSWMD',
+    "meanDirectionalSpread": 'WMDS',
+    "peakDirection": 'WPDI',
+    "peakDirectionalSpread": 'WPDS',
+    "meanPeriod": 'WPFM',
+    "peakPeriod": 'WPPE',
+    "significantWaveHeight": 'WSSH'
+}
