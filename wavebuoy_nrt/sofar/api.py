4d803f0cce0634d3f93f5bdcfda1fdbfe4b5ef8b
Thiago Caminha <caminha.thiago@gmail.com>
2025-01-17 18:00:00 +0800

    removing pysofar use and building requests manually

--- wavebuoy_nrt/sofar/api.py
+++ wavebuoy_nrt/sofar/api.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import requests
 
 from pysofar.sofar import SofarApi
 from pysofar.spotter import Spotter
@@ -8,9 +9,14 @@ import pandas as pd
 class SofarAPI:
     def __init__(self, buoys_metadata: pd.DataFrame):
         self.buoys_metadata = buoys_metadata
-        self.api = self._initiate_sofar_api()
-        self.spotter_grid = self._load_spotter_grid()
-        self.devices = self._load_spotter_devices()
+        # self.api = self._initiate_sofar_api()
+        # self.spotter_grid = self._load_spotter_grid()
+        # self.devices = self._load_spotter_devices()
+
+        self._base_url = "https://api.sofarocean.com/api"
+        self._endpoints = {"waves": "/wave-data",
+                           "latest": "/latest-data",
+                           "devices": "devices"}
 
     def _get_first_token(self):
         return (self.buoys_metadata
@@ -56,8 +62,88 @@ class SofarAPI:
                 idx = devices.index(device)
         return spotter_grid[idx]
     
-    def get_latest_available_datetime(self, spotter_obj) -> datetime:
-        latest_available_datetime = spotter_obj.latest_data()["wave"]["timestamp"]
-        return datetime.strptime(latest_available_datetime, "%Y-%m-%dT%H:%M:%S.%fZ")
+   
+    # ============================
+    def compose_header(self, token:str) -> dict:
+        return {"token" : token}
+    
+    def request_api(self, spot_id: str,
+                     token: str,
+                     query_params: dict,
+                     data_type: str = "waves") -> dict:
+        
+        request_url = self.compose_request_url(base_url=self._base_url, endpoint=self._endpoints[data_type])
+        headers = self.compose_header(token=token)
+        
+        kwargs = {"url":request_url, "headers": headers}
+        if not data_type=="devices":
+            kwargs.update({"params":query_params})
+        response = requests.get(**kwargs)
 
+        if response.status_code == 200:
+            return response.json()["data"]
+        else:
+            # ELABORATE ERROR HANDLING  
+            print(f"Unsuccessfull API call, status {response.status_code}")
+            return
+        
+    def get_latest_available_time(self, spot_id: str, token: str) -> datetime:
+        
+        query_params = self.compose_query_parameters(spot_id=spot_id, add_wave_params=False)
+        response = self.request_api(spot_id=spot_id, token=token, data_type="latest", query_params=query_params)
+        latest_time = response["waves"][-1]["timestamp"]
+        
+        return datetime.strptime(latest_time, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+    
+
+    # def grab_latest_data(self, spot_id: str, token: str) -> dict:
+        
+    #     request_url = self.compose_request_url(base_url=self._base_url, endpoint=self._latest_data_endpoint)
+    #     headers = self.compose_header(token=token)
+    #     response = requests.get(request_url, headers=headers)
+
+    #     if response.status_code == 200:
+    #         return json.loads(response)
+    #     else:
+    #         # ELABORATE ERROR HANDLING  
+    #         print(f"Unsuccessfull API call, status {response.status_code}")
+    #         return
+
+    def get_status_code(self, response: requests.Response) -> int:
+        return response.status_code
+
+    def compose_query_parameters(self,
+                                spot_id: str,
+                                start_date: datetime = None, 
+                                end_date: datetime = None,
+                                add_wave_params: bool = True,
+                                include_surface_temp_data: bool = True,
+                                include_wind_data: bool = True,
+                                include_frequency_data: bool = True,
+                                include_directional_moments: bool = True,
+                                include_partition_data: bool = True,
+                                include_barometer_data: bool = True,
+                                processing_sources="all"):
+        
+        query_params = {"spotterId" : spot_id}
+        
+        if add_wave_params:
+            query_params.update({
+                "startDate": start_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                "endDate": end_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                "includeSurfaceTempData": str(include_surface_temp_data).lower(),
+                "includeWindData": str(include_wind_data).lower(),
+                "includeFrequencyData": str(include_frequency_data).lower(),
+                "includeDirectionalMoments": str(include_directional_moments).lower(),
+                "includePartitionData": str(include_partition_data).lower(),
+                "includeBarometerData": str(include_barometer_data).lower(),
+                "processingSources": processing_sources
+            })
+        
+        return query_params
+    
+    def compose_request_url(self, base_url: str, endpoint: str) -> str:
+        return base_url + endpoint
+        
     
