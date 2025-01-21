32544fbec79adf5ab6af51b22bb69860a73b1f80
Thiago Caminha <caminha.thiago@gmail.com>
2025-01-21 14:09:06 +0800

    working with args, handling smart mooring specifically for grabing sst

--- scripts/cwb-spotter-bulk-netcdf.py
+++ scripts/cwb-spotter-bulk-netcdf.py
@@ -11,7 +11,7 @@ from datetime import datetime, timedelta
 from wavebuoy_nrt.wavebuoy import WaveBuoy
 from wavebuoy_nrt.sofar.api import SofarAPI
 from wavebuoy_nrt.netcdf.lookup import NetCDFFileHandler
-from wavebuoy_nrt.utils import args
+from wavebuoy_nrt.utils import args, IMOSLogging
 
 
 load_dotenv()
@@ -28,16 +28,26 @@ if __name__ == "__main__":
         - qc window from present to past (in hours, days, months, etc)
 
     """
+    vargs = args()
 
-    # vargs = args()
+    # Start logging
+    runtime = datetime.now().strftime("%Y%m%dT%H%M%S")#IMOSLogging().something
+    general_log_file_name = f"{runtime}_general_process.log"
+    IMOSLogging(logging_filepath=os.path.join(vargs.output_path, general_log_file_name)).logging_start()
+    global LOGGER
+    LOGGER = logging.getLogger(__name__)
+
+   
 
     ### TEMPORARY SETUP (REMOVE WHEN DONE)
-    window = 24 # hours
-    window_unit = "hours"
+    window = int(vargs.window)# window = 24 # hours
+    window_unit = vargs.window_unit# window_unit = "hours"
     period_to_qualify = 24 # hours
     backfill = True
+    print(window)
+    print(window_unit)
     ### END OF TEMPORARY SETUP
-
+    
 
     # Loading metadata
     """ 
@@ -67,19 +77,18 @@ if __name__ == "__main__":
     """
 
     for idx, site in wb.buoys_metadata_token_sorted.iterrows():
+       
+        # ### TEMPORARY SETUP TO AVOID UNECESSARY SOFAR API CALLS (REMOVE WHEN DONE)
+        site = wb.buoys_metadata.loc["Hillarys"]
+
+        # ### END OF TEMPORARY SETUP 
         print(site.name)
 
         # Relevant loads ---------------------------------------
-        # sofar_api.check_token_iteration(next_token=wb.buoys_metadata_token_sorted.loc[site.name, 'sofar_token'])
-
-        # spot_id = sofar_api.get_spot_id(site_id=site.name, buoys_metadata=wb.buoys_metadata_token_sorted)
-        # spotter_obj = sofar_api.select_spotter_obj_from_spotter_grid(spot_id=spot_id,
-                                                                # spotter_grid=sofar_api.spotter_grid,
-                                                                # devices=sofar_api.devices)
         
         latest_available_datetime = sofar_api.get_latest_available_time(spot_id=site.serial, token=site.sofar_token)
 
-        window_start_date = wb.generate_window_start_datetime(latest_available_datetime=latest_available_datetime,
+        window_start_time = wb.generate_window_start_time(latest_available_datetime=latest_available_datetime,
                                                               window=window,
                                                               window_unit=window_unit)
 
@@ -94,11 +103,12 @@ if __name__ == "__main__":
                                                         window_unit=window_unit)
             
             latest_nc_file_available = wb.get_latest_nc_file_available(institution=site.region,
-                                                                site_id=site.name)
+                                                                    site_id=site.name)
             latest_processed_datetime = wb.get_latest_processed_datetime(nc_file_path=latest_nc_file_available)
             
             availability_check, nc_file_paths = wb.check_nc_files_needed_available(nc_files_needed=nc_files_needed,
                                                             nc_files_available=nc_files_available)
+            
             if availability_check: # any or all needed ar available
                 nc_to_load = nc_file_paths
             else: # none is available
@@ -106,9 +116,8 @@ if __name__ == "__main__":
                     nc_to_load = latest_nc_file_available
                 else:
                     nc_to_load = None
-
             previous_data_df = wb.load_datasets(nc_file_paths=nc_to_load)
-            start_date = latest_processed_datetime
+            window_start_time = latest_processed_datetime
 
         # Extraction ---------------------------------------
         """
@@ -127,14 +136,24 @@ if __name__ == "__main__":
         #                                 include_partition_data=False,
         #                                 include_barometer_data=True,                                       
         #                                 processing_sources="all")
-        query_params = sofar_api.compose_query_parameters(spot_id=site.serial,
-                                                        start_date=start_date,
-                                                        end_date=latest_available_datetime)
-        new_data_raw = sofar_api.request_api(spot_id=site.serial,
-                                           token=site.sofar_token,
-                                           data_type="waves",
-                                           query_params=query_params)
+        # query_params = sofar_api.compose_query_parameters(spot_id=site.serial,
+        #                                                 start_date=window_start_date,
+        #                                                 end_date=latest_available_datetime)
+        # new_data_raw = sofar_api.request_api(spot_id=site.serial,
+        #                                    token=site.sofar_token,
+        #                                    data_type="waves",
+        #                                    query_params=query_params)
 
+        print(f"start_date: {window_start_time}")
+        print(f"end_date: {latest_available_datetime}")
+
+        new_data_raw = sofar_api.get_wave_data(spot_id=site.serial,
+                                           token=site.sofar_token,
+                                           start_date=window_start_time,
+                                           end_date=latest_available_datetime + timedelta(hours=1))
+
+        print(new_data_raw["surfaceTemp"])
+    
         # Processing ---------------------------------------
         # for parameters_type in ["waves", "wind"]:
         #     pass
@@ -142,9 +161,20 @@ if __name__ == "__main__":
         waves_new_data_df = wb.convert_to_dataframe(raw_data=new_data_raw, parameters_type="waves")
         waves_new_data_df = wb.convert_to_datetime(data=waves_new_data_df)
         
-        
         sst_new_data_df = wb.convert_to_dataframe(raw_data=new_data_raw, parameters_type="surfaceTemp")
         sst_new_data_df = wb.convert_to_datetime(data=sst_new_data_df)
+
+        if sst_new_data_df is None and site.version in ("smart_mooring", "half_smart_mooring"):
+            new_sensor_data_raw = sofar_api.get_sensor_data(spot_id=site.serial,
+                                                        token=site.sofar_token,
+                                                        start_date=window_start_date,
+                                                        end_date=latest_available_datetime + timedelta(hours=1))
+
+            sensor_new_data_df = wb.convert_to_dataframe(raw_data=new_sensor_data_raw)
+            sensor_new_data_df = wb.convert_to_datetime(data=new_sensor_data_raw)
+            
+            sst_new_data_df = wb.get_sst_from_smart_mooring(data=new_sensor_data_raw)
+            
 
         all_new_data_df = wb.merge_parameter_types(waves=waves_new_data_df, sst=sst_new_data_df)
 
