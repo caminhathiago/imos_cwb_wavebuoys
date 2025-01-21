80906b5fea8563133a2c62cb95a5117511de72fb
Thiago Caminha <caminha.thiago@gmail.com>
2025-01-21 14:08:03 +0800

    final adjustments to lookup algorithm, rearranging deprecated methods

--- wavebuoy_nrt/netcdf/lookup.py
+++ wavebuoy_nrt/netcdf/lookup.py
@@ -17,8 +17,6 @@ class NetCDFFileHandler():
     """
     Class used to handle NetCDF files.
     """
-    nc_file_paths_to_process = []
-    latest_processed_datetime = None
     
     def __init__(self):
         pass
@@ -51,59 +49,6 @@ class NetCDFFileHandler():
                                             site_id=site_id+2,
                                             institution=institution)
 
-    def lookup_netcdf_files1(self, 
-                            institution:str,
-                            site_id:str, 
-                            latest_available_datetime:datetime,
-                            minimum_datetime_recursion:datetime=datetime(2020,1,1), # This should be less than the minimum Datetime of the first spotter we ever deployed
-                            window:timedelta=timedelta(hours=24)) -> str:
-
-        
-        if latest_available_datetime < minimum_datetime_recursion:
-            print(f"""No stored data from {minimum_datetime_recursion}.
-                  Considering {site_id} as a new site (i.e. New NetCDF created for {latest_available_datetime.year}-{latest_available_datetime.month}).
-                  """)
-            return
-
-        # nc_file_datetime = latest_available_datetime.replace(day=1).strftime("%Y%m%d") 
-        year = latest_available_datetime.year  
-        month = latest_available_datetime.month
-        nc_file_name = NC_FILE_NAME_TEMPLATE.format(institution=REGION_TO_INSTITUTION[institution],# Temporary data
-                                                    year=year,
-                                                    month=month,
-                                                    site_id=site_id.upper())
-        nc_file_path = os.path.join(FILES_OUTPUT_PATH, nc_file_name)
-
-        if os.path.isfile(nc_file_path):
-            self.nc_file_paths_to_process.append(nc_file_path)
-            nc_dataset = xr.open_dataset(nc_file_path)
-            
-            if self.latest_processed_datetime is None:
-                latest_processed_datetime = self.get_latest_processed_date_time(nc_dataset)
-        
-            period_enough = self.check_period(window=window,
-                                              nc_dataset=nc_dataset,
-                                              latest_available_datetime=latest_available_datetime,
-                                              latest_processed_datetime=latest_processed_datetime)
-            
-            if not period_enough:
-                self.lookup_netcdf_files(institution=institution,
-                                        site_id=site_id,
-                                        latest_available_datetime=latest_available_datetime-timedelta(days=30)
-                                            )
-            
-            return self.nc_file_paths_list 
-        
-        else:
-            print(f"{nc_file_path} does not exist. Probably first data point of the month.")
-            return self.lookup_netcdf_files(institution=institution,
-                                    site_id=site_id,
-                                    latest_available_datetime=latest_available_datetime-timedelta(days=30)
-                                        )       
-
-    # def netcdf_loader(self, nc_file_path:str):
-    #     return xr.open_dataset(nc_file_path)
-
     def lookup_netcdf_files_needed(self, 
                             institution: str,
                             site_id: str, 
@@ -112,40 +57,23 @@ class NetCDFFileHandler():
                             window: int=24,
                             window_unit: str="hours") -> str:
             
-            window_start_date = self.generate_window_start_datetime(latest_available_datetime=latest_available_datetime,
+            window_start_date = self.generate_window_start_time(latest_available_datetime=latest_available_datetime,
                                                                     window=window,
                                                                     window_unit=window_unit)
             monthly_daterange = self._generate_monthly_daterange(start_date=window_start_date,
                                                                 end_date=latest_available_datetime)
-            
+            nc_file_paths_needed = []
             for month in monthly_daterange:
                 nc_file_name = NC_FILE_NAME_TEMPLATE.format(institution=REGION_TO_INSTITUTION[institution],# Temporary data
                                                     monthly_datetime=month.strftime("%Y%m%d"),
                                                     site_id=site_id.upper())
                 print(nc_file_name)
                 nc_file_path = os.path.join(FILES_OUTPUT_PATH, nc_file_name)
-                self.nc_file_paths_to_process.append(nc_file_path)
+                nc_file_paths_needed.append(nc_file_path)
 
-            return self.nc_file_paths_to_process
-
-    def _check_nc_files_exist(self, nc_file_paths_to_process: list) -> list:
-        
-        # Check for all files (best/expected scenario)
-        if all([os.path.exists(f) for f in nc_file_paths_to_process]):
-            return True
-        
-        # Check what files are missing
-        else:
-            missing_files = []
-            for file in nc_file_paths_to_process:
-                if not os.path.exists(file):
-                    print("File exists")
-                    missing_files.append(file)
-
-            return missing_files
-
+            return nc_file_paths_needed
     
-    def generate_window_start_datetime(self,
+    def generate_window_start_time(self,
                                        latest_available_datetime: datetime, 
                                        window: int, 
                                        window_unit: str="hours"):
@@ -165,21 +93,6 @@ class NetCDFFileHandler():
                 monthly_daterange = monthly_daterange.insert(item=window_start_date_str, loc=0)
 
         return monthly_daterange    
-
-    # def check_period(self, 
-    #                 window: timedelta,
-    #                 nc_dataset: xr.Dataset,
-    #                 latest_available_datetime: datetime,
-    #                 latest_processed_datetime: datetime):
-        
-    #     min_datetime = datetime.fromtimestamp(nc_dataset["TIME"].min().values.astype('datetime64[s]').astype(int))
-        
-    #     if (latest_processed_datetime - min_datetime) > window:
-    #         print("Enough data points to be processed.")
-    #         return True 
-    #     else:
-    #         print("NC File does not have enough data points to be processed.")
-    #         return False
 
     def get_available_nc_files(self, institution: str, site_id: str) -> list:
         
@@ -210,13 +123,15 @@ class NetCDFFileHandler():
 
     def check_nc_files_needed_available(self, nc_files_needed: list, nc_files_available: list):
         
-        nc_file_paths = [file in nc_files_available for file in nc_files_needed]
+        check = [file in nc_files_available for file in nc_files_needed]
+        nc_file_paths = [file for file in nc_files_needed if file in nc_files_available]
 
-        if all(nc_file_paths) or any(nc_file_paths):
+        print(nc_file_paths)
+        if all(check) or any(check):
             print("All or some nc files needed are available, load the intersection.")
             availability_check = True
         
-        elif not all(nc_file_paths):
+        elif not all(check):
             availability_check = False
             
         return availability_check, nc_file_paths 
@@ -244,6 +159,30 @@ class NetCDFFileHandler():
 
         return global_dataframe
 
+    def get_site_ids(self, buoys_metadata: pd.DataFrame) -> list:
+        return buoys_metadata.name.list()
+
+    def _validade_site_id(self, site_id: str, site_ids: list) -> bool:
+        if site_id in site_ids:
+            return True
+        else:
+            return False
+
+
+    # def check_period(self, 
+    #                 window: timedelta,
+    #                 nc_dataset: xr.Dataset,
+    #                 latest_available_datetime: datetime,
+    #                 latest_processed_datetime: datetime):
+        
+    #     min_datetime = datetime.fromtimestamp(nc_dataset["TIME"].min().values.astype('datetime64[s]').astype(int))
+        
+    #     if (latest_processed_datetime - min_datetime) > window:
+    #         print("Enough data points to be processed.")
+    #         return True 
+    #     else:
+    #         print("NC File does not have enough data points to be processed.")
+    #         return False
 
 
     # def get_latest_processed_datetime(self, nc_dataset:xr.Dataset):
@@ -255,11 +194,71 @@ class NetCDFFileHandler():
     #                                  .astype(int))
     #                                 )
 
-    def get_site_ids(self, buoys_metadata: pd.DataFrame) -> list:
-        return buoys_metadata.name.list()
+    # def lookup_netcdf_files1(self, 
+    #                         institution:str,
+    #                         site_id:str, 
+    #                         latest_available_datetime:datetime,
+    #                         minimum_datetime_recursion:datetime=datetime(2020,1,1), # This should be less than the minimum Datetime of the first spotter we ever deployed
+    #                         window:timedelta=timedelta(hours=24)) -> str:
 
-    def _validade_site_id(self, site_id: str, site_ids: list) -> bool:
-        if site_id in site_ids:
-            return True
-        else:
-            return False
+        
+    #     if latest_available_datetime < minimum_datetime_recursion:
+    #         print(f"""No stored data from {minimum_datetime_recursion}.
+    #               Considering {site_id} as a new site (i.e. New NetCDF created for {latest_available_datetime.year}-{latest_available_datetime.month}).
+    #               """)
+    #         return
+
+    #     # nc_file_datetime = latest_available_datetime.replace(day=1).strftime("%Y%m%d") 
+    #     year = latest_available_datetime.year  
+    #     month = latest_available_datetime.month
+    #     nc_file_name = NC_FILE_NAME_TEMPLATE.format(institution=REGION_TO_INSTITUTION[institution],# Temporary data
+    #                                                 year=year,
+    #                                                 month=month,
+    #                                                 site_id=site_id.upper())
+    #     nc_file_path = os.path.join(FILES_OUTPUT_PATH, nc_file_name)
+
+    #     if os.path.isfile(nc_file_path):
+    #         self.nc_file_paths_to_process.append(nc_file_path)
+    #         nc_dataset = xr.open_dataset(nc_file_path)
+            
+    #         if self.latest_processed_datetime is None:
+    #             latest_processed_datetime = self.get_latest_processed_date_time(nc_dataset)
+        
+    #         period_enough = self.check_period(window=window,
+    #                                           nc_dataset=nc_dataset,
+    #                                           latest_available_datetime=latest_available_datetime,
+    #                                           latest_processed_datetime=latest_processed_datetime)
+            
+    #         if not period_enough:
+    #             self.lookup_netcdf_files(institution=institution,
+    #                                     site_id=site_id,
+    #                                     latest_available_datetime=latest_available_datetime-timedelta(days=30)
+    #                                         )
+            
+    #         return self.nc_file_paths_list 
+        
+    #     else:
+    #         print(f"{nc_file_path} does not exist. Probably first data point of the month.")
+    #         return self.lookup_netcdf_files(institution=institution,
+    #                                 site_id=site_id,
+    #                                 latest_available_datetime=latest_available_datetime-timedelta(days=30)
+    #                                     )       
+
+    # def netcdf_loader(self, nc_file_path:str):
+    #     return xr.open_dataset(nc_file_path)
+
+    # def _check_nc_files_exist(self, nc_file_paths_to_process: list) -> list:
+        
+    #     # Check for all files (best/expected scenario)
+    #     if all([os.path.exists(f) for f in nc_file_paths_to_process]):
+    #         return True
+        
+    #     # Check what files are missing
+    #     else:
+    #         missing_files = []
+    #         for file in nc_file_paths_to_process:
+    #             if not os.path.exists(file):
+    #                 print("File exists")
+    #                 missing_files.append(file)
+
+    #         return missing_files
