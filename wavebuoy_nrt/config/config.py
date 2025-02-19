20e3a63303ca822f6c8c0d6d882c65786d060015
caminhathiago <caminha.thiago@gmail.com>
2025-02-19 17:01:42 +0800

    spectral templates added

--- wavebuoy_nrt/config/config.py
+++ wavebuoy_nrt/config/config.py
@@ -2,14 +2,14 @@ FILES_OUTPUT_PATH = r"C:\Users\00116827\cwb\wavebuoy_aodn\output_path\final_test
 FILES_PATH = r"C:\Users\00116827\cwb\wavebuoy_aodn\wavebuoy_nrt"
 IRDS_PATH = r"\\drive.irds.uwa.edu.au\OGS-COD-001\CUTTLER_wawaves"
 
-
-NC_FILE_NAME_TEMPLATE = "{operating_institution}_{monthly_datetime}_{site_id}_RT_WAVE-PARAMETERS_monthly.nc"
-
 OPERATING_INSTITUTIONS = {"UWA":"UWA",
                           "Deakin":"VIC-DEAKIN-UNI",
                           "NSW-DCCEEW" : "NSW-DPE",
                           "IMOS":"IMOS_COASTAL-WAVE-BUOYS"
                           }
+
+NC_FILE_NAME_TEMPLATE = "{operating_institution}_{monthly_datetime}_{site_id}_RT_WAVE-PARAMETERS_monthly.nc"
+NC_SPECTRAL_FILE_NAME_TEMPLATE = "{operating_institution}_{monthly_datetime}_{site_id}_RT_WAVE-PARAMETERS_monthly.nc"
 
 AODN_COLUMNS_TEMPLATE = {
     "timestamp": 'TIME',
@@ -24,4 +24,17 @@ AODN_COLUMNS_TEMPLATE = {
     "significantWaveHeight": 'WSSH',
     "degrees": 'TEMP', # Spotter TEMP
     "value": 'TEMP' # SM surface temperature
+}
+
+AODN_SPECTRAL_COLUMNS_TEMPLATE = {
+    "timestamp": 'TIME',
+    "longitude": 'LONGITUDE',
+    "latitude": 'LATITUDE',
+    "a1": 'A1',
+    "b1": 'B1',
+    "a2": 'A2',
+    "b2": 'B2',
+    "varianceDensity": 'ENERGY',
+    "direction": 'DIRECTION',
+    "directionalSpread": 'DIRSPREAD',
 }
