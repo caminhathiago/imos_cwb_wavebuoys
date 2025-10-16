1a6fcb8615478fe7ed1df7206e09fd1de5ac1c5e
caminhathiago <caminha.thiago@gmail.com>
2025-10-16 14:04:25 +0800

    clearing changes

--- setup.py
+++ setup.py
@@ -3,14 +3,11 @@
 
 from setuptools import setup, find_packages
 
-# with open('README.md') as f:
-#     readme = f.read()
-
 def read_requirements(filename):
     with open(filename, encoding="utf-8") as f:
         return [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
-PACKAGE_NAME = 'wavebuoys'
+PACKAGE_NAME = 'imos-coastal-wavebuoys'
 
 PACKAGE_EXCLUDES = [
     "config",
@@ -23,22 +20,25 @@ PACKAGE_EXCLUDES = [
     "netcdf"
 ]
 
+PACKAGE_INCLUDES = [
+    "wavebuoy_nrt*",
+    "wavebuoy_dm*"
+]
+
 PACKAGE_DATA = {
     'wavebuoy.': ['*.csv'],
 }
 
-SCRIPTS = []
-
 setup(
     name=PACKAGE_NAME,
     version='0.1.0',
-    description='Python package to process SD card data to AODN',
+    description='Toolboxes for near real-time and delayed-mode wave buoy data processing',
     author='Thiago Caminha, Michael Cuttler',
     author_email='thiago.caminha@uwa.edu.au, michael.cuttler@uwa.edu.au',
     url='',
     install_requires=read_requirements("requirements.txt"),
-    packages=find_packages(exclude=PACKAGE_EXCLUDES),
-    scripts=SCRIPTS,
+    packages=find_packages(include=PACKAGE_INCLUDES,
+                           exclude=PACKAGE_EXCLUDES),
     package_data=PACKAGE_DATA,
     zip_safe=False,
     python_requires='>3.8'
